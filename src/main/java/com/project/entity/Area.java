c195d526754324217bd6c45d37041d589b0ba6b6
vanh <vietanhnguyen2510@gmail.com>
2024-03-28 16:38:56 +0700

    backup

---
+++ src/main/java/com/project/entity/Area.java
@@ -0,0 +1,29 @@
+package com.project.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "area")
+@Getter
+@Setter
+public class Area {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "building_id")
+    private Building building;
+
+}
