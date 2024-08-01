efcf880ca617b37c1fb3a37123e5e889f4b75b7e
vanh <vietanhnguyen2510@gmail.com>
2024-08-01 15:18:26 +0700

    add lib

--- src/main/resources/static/js/GenerateHeader.js
+++
@@ -1,9 +0,0 @@
-alert("jo")
-alert("Chiều rộng của màn hình là: " + window.innerWidth + " và chiều cao của màn hình là: " + window.innerHeight + ".")
-if (window.innerWidth > window.innerHeight) {
-    alert("Màn hình ngang lớn hơn dọc.");
-} else if (window.innerWidth < window.innerHeight) {
-    alert("Màn hình dọc lớn hơn ngang.");
-} else {
-    alert("Màn hình có tỷ lệ chiều rộng và chiều cao bằng nhau.");
-}
